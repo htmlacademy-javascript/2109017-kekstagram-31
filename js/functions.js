f65eedef1704e9a0766c51fad10435cf0c1ae2c2
Nadi Lysenko <nadirose@mail.ru>
2024-02-21 22:33:04 +0300

    Пишет функции для проверки длины и на палиндром

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,23 @@
-// Функция проверки длины строки.
-const checkLength = (string = '', maxLine = 1) => string.length <= maxLine;
+// Функция для проверки длины строки.
+
+const checkLength = (string = '', maxLine = 1) => (string.length <= maxLine);
 checkLength('проверяемая строка', 20);
+// console.log(checkLength('проверяемая строка', 20));
+
+
+// Функция для проверки является ли строка палиндромом.
+
+const checkPalindrome = (string = '') => {
+  string = string.replaceAll(' ', '').toLowerCase();
+
+  let controlString = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    controlString += string[i];
+  }
+
+  return string === controlString;
+};
+checkPalindrome('Лёша на полке клопа нашёл ');
+// console.log(checkPalindrome('Лёша на полке клопа нашёл '));
+
