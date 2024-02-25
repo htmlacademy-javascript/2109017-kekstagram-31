dbd3e23a07498c052e11c8f9a0831c7b61a92524
Nadi Lysenko <nadirose@mail.ru>
2024-02-25 16:26:18 +0300

    Вносит изменения

--- js/functions.js
+++ js/functions.js
@@ -2,22 +2,28 @@
 
 const checkLength = (string = '', maxLine = 1) => (string.length <= maxLine);
 checkLength('проверяемая строка', 20);
+
 // console.log(checkLength('проверяемая строка', 20));
 
 
 // Функция для проверки является ли строка палиндромом.
 
-const checkPalindrome = (string = '') => {
-  string = string.replaceAll(' ', '').toLowerCase();
-
-  let controlString = '';
-
-  for (let i = string.length - 1; i >= 0; i--) {
-    controlString += string[i];
-  }
-
-  return string === controlString;
+const checkPalindrome = (string) => {
+  const compareString = string.replaceAll(' ', '').toLowerCase();
+  const reverseString = compareString.split('').reverse().join('');
+  return compareString === reverseString;
 };
 checkPalindrome('Лёша на полке клопа нашёл ');
-// console.log(checkPalindrome('Лёша на полке клопа нашёл '));
+
+// способ №2
+// const checkPalindrome = (string = '') => {
+//   string = string.replaceAll(' ', '').toLowerCase();
+//   let controlString = '';
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     controlString += string[i];
+//   }
+//   return string === controlString;
+// };
+// checkPalindrome('Лёша на полке клопа нашёл ');
+// // console.log(checkPalindrome('Лёша на полке клопа нашёл '));
 
